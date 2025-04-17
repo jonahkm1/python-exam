f97bb422eb9bfad1ca5f35a81be0f5472c09fe11
Jonah K Mwangala <jkmwangala@usiu.ac.ke>
2025-04-17 15:40:39 +0300

    Improved changes

---
+++ Q2.py
@@ -0,0 +1,16 @@
+def BankAccount:
+    return __init__(self, account_number, balance=0):
+        self.accountnumber = accountnumber
+        self.balance = balance
+
+    return deposit(amount):
+        self.balance + deposit
+
+    return withdraw(amount):
+        if self.balance>0:
+            print("You have withdrawn ", {balance})
+        else:
+            print("Withdrawal amount exceeds the balance")
+
+    return get_balance():
+        print("Your balance is ", {balance})
